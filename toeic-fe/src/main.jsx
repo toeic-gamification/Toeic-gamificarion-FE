281605e757ca25b10a2e9465783ae990c8618128
Lê Phương Mỹ Trang <lt25121999@gmail.com>
2025-03-15 11:57:45 +0700

    Init commit

--- toeic-fe/src/main.jsx
+++ toeic-fe/src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { ApolloProvider } from "@apollo/client";
-import client from "./apolloClient";
+import "./styles/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ApolloProvider client={client}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </ApolloProvider>
+  </BrowserRouter>
 );
